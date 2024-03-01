f68fa55622ae6589c63617c10157f5a95d43fd03
Haxend <dunaevskyilya@gmail.com>
2024-03-01 03:14:34 +0300

    feat: GetRaservationId

---
+++ src/Test/Booking.Business.Test/Reservation/GetReservationTests.cs
@@ -0,0 +1,26 @@
+﻿using AutoFixture;
+using AutoMapper;
+using Booking.Business.Application.Consumers.Reservation;
+using Booking.Business.Application.Mappings;
+using Booking.Business.Persistence.Context;
+using Booking.Business.Persistence.Repositories;
+using MassTransit.Testing;
+
+
+namespace Booking.Business.Test.Reservation
+{
+    public class GetReservationTests
+    {
+        private GetReservationConsumer Consumer { get; }
+
+        public GetReservationTests()
+        {
+            var config = new MapperConfiguration(cfg => cfg.AddProfile<ReservationMapper>());
+
+            //var reservationRepository = new ReservationRepository(DataContext);
+            //Consumer = new GetReservationConsumer(reservationRepository, new Mapper(config));
+        }
+
+        // Todo
+    }
+}
