8419b32c1ac2ee00465347093ce947e1ec394187
fangarh <dvornikov.d@gmail.com>
2024-03-01 15:59:15 +0300

    Added tables and reservation tests

---
+++ src/Test/Booking.Business.Test/Reservation/GetReservationListTests.cs
@@ -0,0 +1,66 @@
+﻿using AutoMapper;
+using Booking.Business.Application.Consumers.Reservation;
+using Booking.Business.Persistence.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Booking.Business.Application.Mappings;
+using AutoFixture;
+using MassTransit.Testing;
+using Otus.Booking.Common.Booking.Contracts.Filial.Requests;
+using Otus.Booking.Common.Booking.Contracts.Filial.Responses;
+using Otus.Booking.Common.Booking.Contracts.Reservation.Requests;
+using Otus.Booking.Common.Booking.Contracts.Reservation.Responses;
+
+namespace Booking.Business.Test.Reservation
+{
+    public class GetReservationListTests : BaseTest
+    {
+        private GetReservationsListConsumer Consumer { get; }
+
+        public GetReservationListTests()
+        {
+            var config = new MapperConfiguration(cfg => cfg.AddProfile<ReservationMapper>());
+
+            var reservationRepository = new ReservationRepository(DataContext);
+            
+            Consumer = new GetReservationsListConsumer(reservationRepository, new Mapper(config));
+        }
+
+        [Test]
+        public async Task GetReservationList()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                var reservation = Fixture.Build<Domain.Entities.Reservation>().Without(e => e.Table).Create();
+
+                await DataContext.Reservations.AddAsync(reservation);
+            }
+
+            await DataContext.SaveChangesAsync();
+            var testHarness = new InMemoryTestHarness();
+            var consumerHarness = testHarness.Consumer(() => Consumer);
+
+            await testHarness.Start();
+
+            var request = Fixture.Create<GetReservationsList>();
+            request.Offset = 0;
+            request.Limit = 3;
+            // Act
+            await testHarness.InputQueueSendEndpoint.Send(request);
+            var result = testHarness.Published.Select<GetReservationsListResult>().FirstOrDefault()?.Context.Message;
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(testHarness.Consumed.Select<GetReservationsList>().Any(), Is.True);
+                Assert.That(consumerHarness.Consumed.Select<GetReservationsList>().Any(), Is.True);
+                Assert.That(result, Is.Not.Null);
+                Assert.That(result.Reservations.Count, Is.EqualTo(3));
+            });
+
+            await testHarness.Stop();
+        }
+    }
+}
